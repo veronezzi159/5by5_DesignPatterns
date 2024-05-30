139e61b6a51eb30e512844cb0bb217d8c81fff4a
veronezzi159 <lucasveronezzi3@gmail.com>
2024-05-30 09:31:12 -0300

    Add project files.

---
+++ Models/Car.cs
@@ -0,0 +1,12 @@
+﻿namespace Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string  Name { get; set; }
+        public string  Color { get; set; }
+        public int Year { get; set; }
+
+
+    }
+}
